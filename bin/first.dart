1c47d20e48836deaed436005cfc91515f2e7a019
amermuhd98 <amer31nar@gmail.com>
2022-11-13 15:58:07 +0300

    for

--- bin/first.dart
+++ bin/first.dart
@@ -1,15 +1,9 @@
 void main() {
-  String name = "Amer";
-  int age = 14;
-  double hgt = 160;
-  if (name == "Amerh" || age == 140 || hgt == 1600) {// لاتنسا هذا يدل على الخطاءتمام
-    print("$name $age $hgt");
-  } else {
-    print("xdr");
-      print("xdr");
-            print("xdr");
-      print("xdr");
-ssjcncjscj jnxjsj
+  var name=("Amer Muhammed");
+  int x=14;
+  for(x=0; x<6; x++){
+    print("$name $x");
+
 
   }
 }
