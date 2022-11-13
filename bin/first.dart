a323327e2342940e0e8124f02c8845e31d40513b
amermuhd98 <amer31nar@gmail.com>
2022-11-13 14:39:09 +0300

    first commit

---
+++ bin/first.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Amer";
+  int age = 14;
+  double hgt = 160;
+  if (name == "Amerh" || age == 140 || hgt == 1600) {// لاتنسا هذا يدل على الخطاءتمام
+    print("$name $age $hgt");
+  } else {
+    print("xdr");
+  }
+}
