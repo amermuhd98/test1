bfb52a883ec16a1cb9f9ef3791cda3d5b0084747
amermuhd98 <amer31nar@gmail.com>
2022-11-27 13:51:12 +0300

    switch case to school

--- bin/first.dart
+++ bin/first.dart
@@ -1,12 +1,29 @@
 void main() {
-  int amerEven = 0, muhammedOdd = 0, i;
-  for (i = 0; i <= 100; i++) {
-    if (i % 2 == 0){
-      amerEven += i;
-  } else {
-      muhammedOdd+= i;
+  var x= 50/10;
+
+  switch (x.toInt()){
+    case 4:
+    print('49-50 راسب');
+    break;
+    case 6:
+    print('51-59 مقبول');
+    break;
+    case 7:
+    print('60-70 متوسط');
+    break;
+    case 8:
+    print('71-80 جيد جدا');
+    break;
+    case 10:
+    print('90-100 امتياز');
+    break;
+      case 5:
+    print('51-533333339 مقبول');
+    break;
+    default:
+    print('erorr');
+    break;
+  }   
+  
 }
-}
-  print("amer Even ; $amerEven");
-  print("muhammed Odd ; $muhammedOdd");
-  }
+
